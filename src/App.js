6a82d36bb06c0b48de67640bc9829d5df06214b5
Elsterka <38614398+Elsterka@users.noreply.github.com>
2025-03-25 19:02:32 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Letters from "./components/Letters";
+import Body from './components/Body'
+
+
+function App(){
+  return(
+    <div>
+      <Header/>
+      <Letters/>
+      <Body/>      
+    </div>
+  );
+
+}
+
+export default App;
